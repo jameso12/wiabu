77c69f898a05a24aea91d63680afbd65f63f71de
jameso12 <jamesorodz@gmail.com>
2022-01-29 01:24:07 -0400

    some progress was made

--- wiabu.py
+++ wiabu.py
@@ -1,2 +1,32 @@
-if __name__ == "__main__":
-    pass
+from http import client
+from pydoc import cli
+import requests, json,discord, os, time, random
+from dotenv import load_dotenv
+
+# load token
+load_dotenv('D:\code\discordBots\wiabu\.env')
+TOKEN = os.environ.get('wiabuKey')
+
+
+# get env var for bot key
+def quoteGenerator():
+    r = requests.get("https://animechan.vercel.app/api/random")
+    requestDictionary = json.loads(r.text)
+    print(requestDictionary)
+    anime, character, qoute = requestDictionary["anime"],requestDictionary["character"],requestDictionary["quote"]
+    
+    return f'"{qoute}" \n-{character}\n{anime}'
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    await message.channel.send(quoteGenerator())
+
+client.run(TOKEN)
+
+# random.randrange(1,10)
+# time snippet
+# now = datetime.now()
+# current_time = now.strftime("%H:%M:%S") # 07:41:19
+# print("Current Time =", current_time)
