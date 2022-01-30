7ad773a848bf895be5f30039a586c23f84646101
jameso12 <jamesorodz@gmail.com>
2022-01-30 16:49:25 -0400

    documented

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Wiabu Bot
 
-This bot will use an [API](https://animechan.vercel.app/guide) to send random anime quotes
+This bot will use an [API](https://animechan.vercel.app/guide) to send random anime quotes.
+[Deployment on heroku](https://github.com/audieni/discord-py-heroku)
+I forgot I was tracking notes.txt ;) feel free to ignore it.
+
+## Notes
+
+I used dot env to develop nd test locally.
--- wiabu.py
+++ wiabu.py
@@ -6,18 +6,20 @@ from discord.ext import commands
 
 # load token
 load_dotenv('D:\code\discordBots\wiabu\.env')
-TOKEN = os.environ.get('wiabuKey')
-if not TOKEN:
-    TOKEN = os.getenv("KEY") # my key name in heroku
+TOKEN = os.environ.get('wiabuKey') # gets envoriment variable, in my case it is named wiabuKey
+if not TOKEN:   # If token is empty(will happen when I deploy on heroku) get the enviroment var from the os
+    TOKEN = os.getenv("KEY") # My key name in heroku
 
 # get env var for bot key
 def quoteGenerator():
-    r = requests.get("https://animechan.vercel.app/api/random")
-    requestDictionary = json.loads(r.text)
-    print(requestDictionary)
-    anime, character, qoute = requestDictionary["anime"],requestDictionary["character"],requestDictionary["quote"]
+    """ Gets a random qoute from the API and returns a formted f-string that cointains the quote and its details. """
+    r = requests.get("https://animechan.vercel.app/api/random") # Get squote, which is on json format
+    requestDictionary = json.loads(r.text) # Convert to python dictionary(now we can easily use it on our code).
+    # print(requestDictionary) # I used this for testing before I coded the rest of the bot.
+    # Bellow the assigned the values the variables to keep the code clean. 
+    anime, character, quote = requestDictionary["anime"],requestDictionary["character"],requestDictionary["quote"]
     
-    return f'"{qoute}" \n-{character}\n{anime}'
+    return f'"{quote}" \n-{character}\n{anime}'
 
 bot = discord.ext.commands.Bot(command_prefix='!')
 # tow ways of making a bot: discord.Cient or discord.ext.commands.Bot(command_prefix='!')
@@ -25,6 +27,7 @@ bot = discord.ext.commands.Bot(command_prefix='!')
 @bot.command(name="quote")
 async def animeQuote(ctx):
         await ctx.send(quoteGenerator())
+        
 if __name__ == "__main__":
     bot.run(TOKEN)
 
