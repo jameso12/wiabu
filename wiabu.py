f3c0a98392a3ff2d6daefed28aaec3eccbc482b8
jameso12 <jamesorodz@gmail.com>
2022-02-06 23:04:16 -0400

    added exception handling

--- wiabu.py
+++ wiabu.py
@@ -1,5 +1,6 @@
 from http import client
 from pydoc import cli
+from urllib.error import HTTPError
 import requests, json,discord, os, time, random
 from dotenv import load_dotenv
 from discord.ext import commands
@@ -13,19 +14,22 @@ if not TOKEN:   # If token is empty(will happen when I deploy on heroku) get the
 # get env var for bot key
 def quoteGenerator():
     """ Gets a random qoute from the API and returns a formted f-string that cointains the quote and its details. """
-    r = requests.get("https://animechan.vercel.app/api/random") # Get squote, which is on json format
-    requestDictionary = json.loads(r.text) # Convert to python dictionary(now we can easily use it on our code).
-    # print(requestDictionary) # I used this for testing before I coded the rest of the bot.
-    # Bellow the assigned the values the variables to keep the code clean. 
-    anime, character, quote = requestDictionary["anime"],requestDictionary["character"],requestDictionary["quote"]
-    
-    return f'"{quote}" \n-{character}\n{anime}'
+    try:
+        r = requests.get("https://animechan.vercel.app/api/random") # Get squote, which is on json format
+        requestDictionary = json.loads(r.text) # Convert to python dictionary(now we can easily use it on our code).
+        # Bellow the assigned the values the variables to keep the code clean. 
+        anime, character, quote = requestDictionary["anime"],requestDictionary["character"],requestDictionary["quote"]
+        rString = f'"{quote}" \n-{character}\n{anime}'
+    except HTTPError as err:
+        rString = err
+    return "Something went wrong!\nData for nerds:\n"+rString
 
 bot = discord.ext.commands.Bot(command_prefix='!')
 # tow ways of making a bot: discord.Cient or discord.ext.commands.Bot(command_prefix='!')
 
 @bot.command(name="quote")
 async def animeQuote(ctx):
+        #TODO change the function call quoteGenerator so that this works
         await ctx.send(quoteGenerator())
         
 if __name__ == "__main__":
